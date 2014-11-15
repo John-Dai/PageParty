71c510e31d1caa7cebf84132cd8a08059c65e7fa
John Dai <johnrundai@gmail.com>
2014-11-15 18:24:48 -0500

    Simple seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+VoteLine.delete_all
+VoteLine.create(:code=>'<h1>This is heading 1</h1>',
+		:votes=>0)
+VoteLine.create(:code=>'<h2>This is heading 2</h2>',
+		:votes=>0)
+VoteLine.create(:code=>'<h3>This is heading 3</h3>',
+		:votes=>0)
+VoteLine.create(:code=>'<h4>This is heading 4</h4>',
+		:votes=>0)
+VoteLine.create(:code=>'<h5>This is heading 5</h5>',
+		:votes=>0)
